8c1160033218743a78e2093e1db4cef362ea9853
malasbooks <bookmala30@gmail.com>
2026-04-03 19:10:17 -0400

    Update scriptpersonal.js

--- scriptpersonal.js
+++ scriptpersonal.js
@@ -42,3 +42,28 @@ sections.forEach(section =>
     observer.observe(section);
 });
 
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () =>
+{
+    let current = "";
+
+    document.querySelectorAll("section").forEach(section =>
+    {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop)
+        {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link =>
+    {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current)
+        {
+            link.classList.add("active");
+        }
+    });
+});
+
